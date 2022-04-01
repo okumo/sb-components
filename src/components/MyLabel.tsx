747a898547889a592c8cec24fe63b8a47181a6ec
Oscar <oscar.kballero@hotmail.com>
2022-03-31 23:13:30 -0500

    chromatic agregado

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,55 +1,52 @@
-import './mylabel.css';
+import "./mylabel.css";
 
 export interface MyLabelProps {
-    /**
-    * Este es el mensaje a mostrar en la etiqueta
-    */
-    label: string;
-    /**
-     * Este es el tamaño de la etiqueta
-    */
-    size: 'normal'|'h1'|'h2'|'h3';
+  /**
+   * Este es el mensaje a mostrar en la etiqueta
+   */
+  label: string;
+  /**
+   * Este es el tamaño de la etiqueta
+   */
+  size: "normal" | "h1" | "h2" | "h3";
 
-    /**
-     * Si quiere todo capitalizado
-    */
-    allCaps?: boolean;
+  /**
+   * Si quiere todo capitalizado
+   */
+  allCaps?: boolean;
 
-    /**
-     * Colores básicos del botón
-    */
-    color?: 'primary'|'secondary'|'tertiary';
+  /**
+   * Colores básicos del botón
+   */
+  color?: "primary" | "secondary" | "tertiary";
 
-    /**
-     * Color personalizado de la fuente
-    */
-    fontColor?: string;
+  /**
+   * Color personalizado de la fuente
+   */
+  fontColor?: string;
 
-    /**
-     * Color personalizado de la fuente
-    */
-    backgroundColor?: string;
-
+  /**
+   * Color personalizado de la fuente
+   */
+  backgroundColor?: string;
 }
-
-
 
 export const MyLabel = ({
-    allCaps = false,
-    color   = 'primary',
-    label   = 'No Label',
-    size    = 'normal',
-    fontColor,
-    backgroundColor = 'transparent'
+  allCaps = false,
+  color = "primary",
+  label = "This is a label",
+  size = "normal",
+  fontColor,
+  backgroundColor = "transparent",
 }: MyLabelProps) => {
-    return (
-        <span 
-            className={`label ${ size } text-${ color }`}
-            style={{ color: fontColor, backgroundColor }}>
-            { allCaps ? label.toUpperCase() : label }
-        </span>
-    )
-}
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor, backgroundColor: backgroundColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
 
 export default MyLabel;
-
